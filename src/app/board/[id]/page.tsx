78a5d776ec255d6bf7fef330555529db93c9edb7
Oscar Dejesus <oscar6dejesus@gmail.com>
2025-09-25 19:19:16 -0400

    editing pages

---
+++ src/app/board/[id]/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Board() {
+
+  return (
+   
+    <div className="container">
+      <div className="card">
+
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 // src/app/page.tsx
 export default function Home() {
   return (
-    <main className="card">
-      <h1>Welcome to the Kanban Board</h1>
-    </main>
+    <></>
   );
 }
